0daf21a3735b3a9c5eb4a6621291d614ee8888b9
Jafeer <jafeeralin@gmail.com>
2018-11-17 19:04:49 +0530

    action post

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,9 +1,11 @@
 import {registerMessage} from '../controller/contactController.js';
 export default (server) => {
     server.get('/submitAction', function(req, res){
+        console.log(req)
         registerMessage(req,res);
       });
       server.post('/submitAction1', function(req, res){
+        console.log(req)
         registerMessage(req,res);
       });
 }
